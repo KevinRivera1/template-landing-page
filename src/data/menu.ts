bf1690e297e9eabcc09629e8752244c0605010cf
KevinRivera1 <shimpo11.kr@gmail.com>
2025-03-29 15:25:57 -0500

    [FEAT]: Implement dynamic Navbar using menu data from external file

---
+++ src/data/menu.ts
@@ -0,0 +1,22 @@
+export const Menu = [
+	{
+		name: 'Home',
+		link: 'index.html',
+	},
+	{
+		name: 'About',
+		link: 'about.html',
+	},
+	{
+		name: 'Contact',
+		link: 'contact.html',
+	},
+	{
+		name: 'Services',
+		link: 'services.html',
+	},
+	{
+		name: 'Portfolio',
+		link: 'portfolio.html',
+	},
+];
