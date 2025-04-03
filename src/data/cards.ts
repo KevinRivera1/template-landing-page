9a15be07af3147224ea507573231296efa630ef2
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-03 18:55:27 -0500

    [STYLE]: Improve layout and spacing in Articles component for better presentation

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -2,7 +2,8 @@ export const Cards = [
 	{
 		image: 'images/1.jpg',
 		title: 'Layered security',
-		description: 'whit layered security, we ensure the safety of every unit we provide.',
+		description:
+			'whit layered security, we ensure the safety of every unit we provide.',
 		link: 'articles/understanding-typescript.html',
 	},
 	{
@@ -15,25 +16,29 @@ export const Cards = [
 	{
 		image: 'images/1.jpg',
 		title: 'Reliable customer service',
-		description: 'we are always ready to help you with any questions or problems you may have.',
+		description:
+			'we are always ready to help you with any questions or problems you may have.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
 		image: 'images/1.jpg',
 		title: 'Maintenance manual book',
-		description: 'we provide a maintenance manual book for each unit we provide, so you can be sure of the quality of the whole.',
+		description:
+			'we provide a maintenance manual book for each unit we provide, so you can be sure of the quality of the whole.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
 		image: 'images/1.jpg',
 		title: 'Delivered safely',
-		description: 'we ensure that every unit we provide is delivered safely and securely.',
+		description:
+			'we ensure that every unit we provide is delivered safely and securely.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
 		image: 'images/1.jpg',
 		title: 'Based on artificial intelligence',
-		description: 'we use artificial intelligence to ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
+		description:
+			'we use artificial intelligence to ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
 		link: 'articles/building-react-app-with-typescript.html',
-	}
+	},
 ];
