48b7e5a6057b00ce1f885608f94b0a7b9eda4d88
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-03 18:52:51 -0500

    [FEAT]: Update Articles component with new card layout and enhanced content

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,21 +1,39 @@
 export const Cards = [
 	{
 		image: 'images/1.jpg',
-		title: 'Understanding TypeScript',
-		description: 'A deep dive into TypeScript and its features.',
+		title: 'Layered security',
+		description: 'whit layered security, we ensure the safety of every unit we provide.',
 		link: 'articles/understanding-typescript.html',
 	},
 	{
 		image: 'images/1.jpg',
-		title: 'JavaScript vs TypeScript',
+		title: 'Quality control of each part',
 		description:
-			'Comparing JavaScript and TypeScript for modern web development.',
+			'we ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
 		link: 'articles/javascript-vs-typescript.html',
 	},
 	{
 		image: 'images/1.jpg',
-		title: 'Building a React App with TypeScript',
-		description: 'Step-by-step guide to building a React app using TypeScript.',
+		title: 'Reliable customer service',
+		description: 'we are always ready to help you with any questions or problems you may have.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
+	{
+		image: 'images/1.jpg',
+		title: 'Maintenance manual book',
+		description: 'we provide a maintenance manual book for each unit we provide, so you can be sure of the quality of the whole.',
+		link: 'articles/building-react-app-with-typescript.html',
+	},
+	{
+		image: 'images/1.jpg',
+		title: 'Delivered safely',
+		description: 'we ensure that every unit we provide is delivered safely and securely.',
+		link: 'articles/building-react-app-with-typescript.html',
+	},
+	{
+		image: 'images/1.jpg',
+		title: 'Based on artificial intelligence',
+		description: 'we use artificial intelligence to ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
+		link: 'articles/building-react-app-with-typescript.html',
+	}
 ];
