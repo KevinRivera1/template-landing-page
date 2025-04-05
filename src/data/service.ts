947a38f24edc7694cc3c38702b77d8591ffad950
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-05 14:10:07 -0500

    [FEATURE]: Add Services section with service offerings and arrow-up icon

---
+++ src/data/service.ts
@@ -0,0 +1,18 @@
+export const Service = [
+	{
+		id: 1,
+		name: 'Solar panels for home',
+	},
+	{
+		id: 2,
+		name: 'Solar panels for industry',
+	},
+	{
+		id: 3,
+		name: 'Solar panels for chargers',
+	},
+	{
+		id: 4,
+		name: 'wind power generator',
+	},
+];
