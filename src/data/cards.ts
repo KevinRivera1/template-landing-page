4cbf9784f53af34be5b1f3895846b6f40d072d4f
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-06 12:28:14 -0500

    [STYLE]: Update image sources in Cards data to use imported SVG assets for consistency and improved quality

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,41 +1,48 @@
+import Layer from '@assets/svg/layer.svg';
+import Quality from '@assets/svg/quality.svg';
+import Message from '@assets/svg/message.svg';
+import Maintenance from '@assets/svg/book.svg';
+import Delivered from '@assets/svg/delivered.svg';
+import Radar from '@assets/svg/radar.svg';
+
 export const Cards = [
 	{
-		image: 'images/1.jpg',
+		image: Layer.src,
 		title: 'Layered security',
 		description:
 			'whit layered security, we ensure the safety of every unit we provide.',
 		link: 'articles/understanding-typescript.html',
 	},
 	{
-		image: 'images/1.jpg',
+		image: Quality.src,
 		title: 'Quality control of each part',
 		description:
 			'we ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
 		link: 'articles/javascript-vs-typescript.html',
 	},
 	{
-		image: 'images/1.jpg',
+		image: Message.src,
 		title: 'Reliable customer service',
 		description:
 			'we are always ready to help you with any questions or problems you may have.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
-		image: 'images/1.jpg',
+		image: Maintenance.src,
 		title: 'Maintenance manual book',
 		description:
 			'we provide a maintenance manual book for each unit we provide, so you can be sure of the quality of the whole.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
-		image: 'images/1.jpg',
+		image: Delivered.src,
 		title: 'Delivered safely',
 		description:
 			'we ensure that every unit we provide is delivered safely and securely.',
 		link: 'articles/building-react-app-with-typescript.html',
 	},
 	{
-		image: 'images/1.jpg',
+		image: Radar.src,
 		title: 'Based on artificial intelligence',
 		description:
 			'we use artificial intelligence to ensure the quality of each part we provide, so that you can be sure of the quality of the whole.',
