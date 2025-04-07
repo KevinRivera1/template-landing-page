50a9b35c6b26935e12be041a2be8bd3f64f66c4f
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-07 17:41:46 -0500

    UPDATE: Configure Astro site settings and integrate sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://template-landing-page.vercel.app/',
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	experimental: {
 		svg: {
 			mode: 'sprite',
 		},
 	},
+
+	integrations: [sitemap()],
 });
