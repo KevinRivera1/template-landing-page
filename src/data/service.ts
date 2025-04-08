befbd924cef649dc527f95c59dd163dac08020cb
KevinRivera1 <shimpo11.kr@gmail.com>
2025-04-08 16:40:40 -0500

    UPDATE: Refactor service data structure and integrate into Services component for dynamic content rendering

--- src/data/service.ts
+++ src/data/service.ts
@@ -1,18 +1,41 @@
-export const Service = [
-	{
-		id: '01',
-		name: 'Solar panels for home',
+export const serviceData = {
+	header: {
+		title: 'Trusted service,',
+		highlight: 'for your various needs',
 	},
-	{
-		id: '02',
-		name: 'Solar panels for industry',
+	button: {
+		text: 'Get in touch',
+		url: '/',
+		ariaLabel: 'Get in touch service',
 	},
-	{
-		id: '03',
-		name: 'Solar panels for chargers',
+	services: [
+		{
+			id: '01',
+			name: 'Solar panels for home',
+			detailsText: 'View Details',
+			detailsUrl: '/',
+		},
+		{
+			id: '02',
+			name: 'Solar panels for industry',
+			detailsText: 'View Details',
+			detailsUrl: '/',
+		},
+		{
+			id: '03',
+			name: 'Solar panels for chargers',
+			detailsText: 'View Details',
+			detailsUrl: '/',
+		},
+		{
+			id: '04',
+			name: 'Wind power generator',
+			detailsText: 'View Details',
+			detailsUrl: '/',
+		},
+	],
+	featureImage: {
+		url: 'https://images.unsplash.com/photo-1676337167328-5491d30de8f4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		alt: 'Background service image',
 	},
-	{
-		id: '04',
-		name: 'wind power generator',
-	},
-];
+};
